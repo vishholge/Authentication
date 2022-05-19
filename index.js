ae98347615ae1679b04fb654ae8d915a83e17655
vishwanath <vishwanathholge18@gmail.com>
2022-05-19 10:09:00 +0530

    added some js part

--- index.js
+++ index.js
@@ -30,44 +30,79 @@ function status(activebar){
      
     switch(activebar){
 		case 'HOME':
-			homenavlink.classList.add("active")
-			homePage.style.display = 'block'
+			homenavlink.classList.add("active");
+			homePage.style.display = 'block';
+      // console.log("Hello world");
 			break
 		case 'SIGNUP':
-			signupnavlink.classList.add("active")
-			signupPage.style.display = 'block'
+			signupnavlink.classList.add("active");
+			signupPage.style.display = 'block';
+     // console.log("Hello world");
 			break
 		case 'LOGIN':
-			loginnavlink.classList.add("active")
-			loginPage.style.display = 'block'
+			loginnavlink.classList.add("active");
+			loginPage.style.display = 'block';
 			break
     }
 
 }
-let signuppassword = document.getElementById("sign-up-password");
+// Authetication code:-
+let signupPassword = document.getElementById("sign-up-password");
 
 let loginPassword = document.getElementById("log-in-password");
 let loginId = document.getElementById("log-in-id");
 
+const lookup = {
+  'A': 'N', 'B': 'O', 'C': 'P', 'D': 'Q',
+  'E': 'R', 'F': 'S', 'G': 'T', 'H': 'U',
+  'I': 'V', 'J': 'W', 'K': 'X', 'L': 'Y',
+  'M': 'Z', 'N': 'A', 'O': 'B', 'P': 'C',
+  'Q': 'D', 'R': 'E', 'S': 'F', 'T': 'G',
+  'U': 'H', 'V': 'I', 'W': 'J', 'X': 'K',
+  'Y': 'L', 'Z': 'M',
+  'a': 'n', 'b': 'o', 'c': 'p', 'd': 'q',
+  'e': 'r', 'f': 's', 'g': 't', 'h': 'u',
+  'i': 'v', 'j': 'w', 'k': 'x', 'l': 'y',
+  'm': 'z', 'n': 'a', 'o': 'b', 'p': 'c',
+  'q': 'd', 'r': 'e', 's': 'f', 't': 'g',
+  'u': 'h', 'v': 'i', 'w': 'j', 'x': 'k',
+  'y': 'l', 'z': 'm',
+  '0': '5', '1': '6', '2': '7', '3': '8',
+  '4': '9', '5': '0', '6': '1', '7': '2',
+  '8': '3', '9': '4',
+  '!': '#', '$': '%', '&': '+', '-': '@',
+  '_': '~', '#': '!', '%': '$', '+': '&',
+  '@': '-', '~': '_'
+}
+
 const encode = (inputStr) => {
    let codeArr = inputStr.split("");
    const lookupKeys = Object.keys(lookup);
+   //console.log(lookupKeys);
    const lookUpValues = Object.values(lookup);
+   //console.log(lookUpValues);
    let encodeArr = codeArr.map(codeChar =>{
-       let index = lookUpValues.find(element => element === codeChar);
+       let index = lookUpValues.indexOf(codeChar);
+      // console.log(index);
        return lookupKeys[index];
    })
-   encodeArr.join("");
+   //console.log(encodeArr);
+   //console.log(encodeArr.join(""));
+    return encodeArr.join("");
 }
 const decode = (encodeStr) => {
-    const codeArr = endcodeStr.split("");
-    let decodeArr = codeArr.map(codeChar => loolup[codeChar]);
+    const codeArr = encodeStr.split("");
+    let decodeArr = codeArr.map(codeChar => lookup[codeChar]);
+    //console.log(decodeArr);
     return decodeArr.join("");
 }
+let validAlert = document.getElementById("valid-alert");
 
 const signUp = () => {
-    let inputpassword = signuppassword.value; 
-    state.password = encode(inputpassword);
+    let inputPassword = signupPassword.value; 
+    console.log(inputPassword);
+    state.password = encode(inputPassword);
+    console.log(state.password);
     validAlert.innerText = 'Password successfully changed!'
 	validAlert.style.display = 'block'
 	setTimeout(() => {
@@ -75,7 +110,10 @@ const signUp = () => {
 	}, 3000)
 
 }
+let loginValid = document.getElementById("loginValid");
+let longinValid = document.getElementById("loginInvalid");
 const logIn = () => {
+  console.log("hello word");
     let inputPassword = loginPassword.value
   if(inputPassword === decode(state.password)){
     loginValid.style.display = 'block'
@@ -84,4 +122,28 @@ const logIn = () => {
     loginInvalid.style.display = 'block'
     loginValid.style.display = 'none'
   }
+}
+
+
+// for cofirming password
+
+
+
+const confirmPass = () => {
+  let confirmPassword = document.getElementById("confirm-password");
+  let selectspan = document.getElementById("confirm-pass");
+  let confirmPass = confirmPassword.value;
+  let inputPass = signupPassword.value;
+  if(inputPass === confirmPass){
+    selectspan.style.backgroundColor ="green";
+    selectspan.style.display = "block";
+    
+
+  }
+  else{
+    selectspan.style.backgroundColor = "red";
+    selectspan.style.display = "block";
+    
+  }
+
 }
